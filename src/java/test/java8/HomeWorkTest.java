4855774b07a1b4a4cec7c9e96595dd94db947ac9
Yura <yuriy.petrashenko7@gmail.com>
2020-04-13 21:33:52 +0300

    Add test for task1

--- src/java/test/java8/HomeWorkTest.java
+++ src/java/test/java8/HomeWorkTest.java
@@ -3,6 +3,7 @@ package java8;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -15,6 +16,18 @@ public class HomeWorkTest {
         List<String> names = Arrays.asList("Diana", "Yura", "Vasya", "Anton", "Katya", "Daria", "Sveta", "Tom");
 
         String expected = "1. Yura, 3. Anton, 5. Daria, 7. Tom";
+        String actual = homeWork.task1(names);
+
+        System.out.println(actual);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnEmpty() {
+        HomeWork homeWork = new HomeWork();
+        List<String> names = Collections.emptyList();
+
+        String expected = "";
         String actual = homeWork.task1(names);
 
         System.out.println(actual);
